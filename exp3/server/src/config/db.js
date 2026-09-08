d4757055198977bff39822ee14e844edb88901d3
Ayush Dobey <ayushdobey@gmail.com>
2026-09-08 10:58:05 +0530

    Delete exp3 directory

--- exp3/server/src/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose';
-
-export async function connectDatabase() {
-  await mongoose.connect(process.env.MONGODB_URI, { serverSelectionTimeoutMS: 5000 });
-  console.log(`MongoDB connected: ${mongoose.connection.host}`);
-}
